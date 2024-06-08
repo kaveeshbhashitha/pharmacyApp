8ddcbfc3f49a41e5d5530117cd39f9be515ffb20
Bhashitha Kaveesh <159679734+kaveeshbhashitha@users.noreply.github.com>
2024-06-08 11:24:20 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
 {
     //function for authentication navigation
+    //this controller manage user navigations for admin and other users
+    //keep this secure and you can add more users and user levels based on the role of system
     public function index(){
         if (Auth::user() -> userrole == 'user') {
             return view('user.home');
